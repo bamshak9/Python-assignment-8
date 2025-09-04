d2b291cb8b9397d7940ebd30876aeaba00a9adbe
bamshak9 <b.fwangyil@alustudent.com>
2025-09-04 20:15:43 +0100

    added functionality to library class

--- library.py
+++ library.py
@@ -19,3 +19,29 @@ Example Usage:
     lib.return_book("Alice")
     print(lib.show_available_books())  # ["Data Science Handbook", "Python 101"]
 """
+class Library:
+    def __init__(self):
+        self.available=[]
+        self.borrowed={}
+    def add_book(self,book):
+        self.available.append(book)
+    def borrow_book(self,user,book):
+        self.available.remove(book)
+        self.borrowed[user]=book
+    def return_book(self,user):
+        if user in self.borrowed.keys():
+            self.available.append(self.borrowed[user])
+            self.borrowed.pop(user)
+        else:
+            print(f"{user} did not borrow a book")
+    def show_available_books(self):
+        return self.available
+
+lib = Library()
+lib.add_book("Python 101")
+lib.add_book("Data Science Handbook")
+lib.borrow_book("Alice", "Python 101")
+print(lib.show_available_books())
+lib.return_book("Alice")
+lib.return_book("Alice")
+print(lib.show_available_books())
