088c518b2ddb631fa427d346bcb21abf35e371c0
bamshak9 <b.fwangyil@alustudent.com>
2025-09-04 19:11:19 +0100

    added functionality to bus class in transport file

--- transport.py
+++ transport.py
@@ -15,3 +15,30 @@ Example Usage:
     metro_bus.add_passenger("Lisa")  # "Bus is full!"
     print(bus.show_passengers())  # ["John", "Mary", "Paul"]
 """
+class Bus:
+    def __init__(self, **capacity):
+        self.capacity= capacity.get("capacity", 0)
+        self.passengers=[]
+    def add_passenger(self, passenger):
+        if len(self.passengers)>=self.capacity:
+            print("Bus is full")
+        else:
+            self.passengers.append(passenger)
+    def remove_passenger(self, passenger):
+        if passenger in self.passengers:
+            self.passengers.remove(passenger)
+        else:
+            print("passenger not in the bus")
+    def show_passengers(self):
+        return self.passengers
+
+
+
+metro_bus = Bus(capacity=3)
+print(metro_bus.capacity)
+metro_bus.add_passenger("John")
+metro_bus.add_passenger("Mary")
+metro_bus.add_passenger("Paul")
+metro_bus.remove_passenger("Matt")
+metro_bus.add_passenger("Lisa")
+print(metro_bus.show_passengers())
