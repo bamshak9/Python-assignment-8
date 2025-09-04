9402a1c16341d5e85915fa5494278fe0fcbe31fe
bamshak9 <b.fwangyil@alustudent.com>
2025-09-04 19:47:04 +0100

    added functionality to order class in restaurant file

--- restaurant.py
+++ restaurant.py
@@ -15,3 +15,32 @@ Example Usage:
     order.add_item("Drink", 3)
     print(order.calculate_total())  # 5500
 """
+class RestaurantOrder:
+    def __init__(self, prices):
+        self.prices= prices
+        self.orders={}
+    def add_item(self,item, quantity):
+        self.orders[item]=quantity
+    def remove_item(self,item):
+        if item in self.orders.keys():
+            self.orders.pop(item)
+        else:
+            print("Item not ordered")
+    def calculate_total(self):
+        total=0
+        for item in self.orders:
+            total+=(self.orders[item]*self.prices[item])
+        return total
+    def show_current_order(self):
+        print(self.orders)
+    
+    
+
+
+my_prices={"Pizza": 2000, "Burger": 1500, "Drink": 500}
+order = RestaurantOrder(my_prices)
+order.show_current_order()
+order.add_item("Pizza",2)
+order.add_item("Drink", 3)
+order.show_current_order()
+print(order.calculate_total())
