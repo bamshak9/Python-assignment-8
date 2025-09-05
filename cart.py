238b8db66dc3dbb146aae7989fcd6ca576bef6d8
bamshak9 <b.fwangyil@alustudent.com>
2025-09-06 00:28:46 +0100

    added functionality to ShoppingCart class in class file

--- cart.py
+++ cart.py
@@ -16,3 +16,36 @@ Example Usage:
     cart.remove_item("Shirt", 1)
     print(cart.calculate_total())  # 5000
 """
+class ShoppingCart:
+    def __init__(self,prices):
+        self.prices=prices
+        self.items={}
+    def add_item(self,item,quantity):
+        if item in self.items.keys():
+            self.items[item]+=quantity
+        else:
+            self.items[item]=quantity
+    def remove_item(self, item, quantity):
+        if item in self.items.keys():
+            if quantity>=self.items[item]:
+                self.items.pop(item)
+            else:
+                self.items[item]-=quantity
+        else:
+            print("Item not in cart")
+    def clear_cart(self):
+        self.items.clear()
+    def calculate_total(self):
+        cost=0
+        for item in self.items:
+            cost+=(self.items[item]*self.prices[item])
+        return cost
+    
+
+
+prices = {"Shirt": 5000, "Shoes": 12000}
+cart = ShoppingCart(prices)
+cart.add_item("Shirt", 2)
+print(cart.calculate_total())
+cart.remove_item("Shirt", 1)
+print(cart.calculate_total())
