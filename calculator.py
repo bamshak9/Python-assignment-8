35d374b5e39be8acba170048429beed05953096e
bamshak9 <b.fwangyil@alustudent.com>
2025-09-05 23:45:49 +0100

    added functionality to calculator class

--- calculator.py
+++ calculator.py
@@ -12,3 +12,23 @@ Example Usage:
     print(calc.add(5, 3))        # 8
     print(calc.divide(10, 0))    # "Error: Division by zero"
 """
+class Calculator:
+    def __init__(self):
+        self.operations="Addition, multiplcation, subtraction, division"   
+    def add(self, a,b):
+        return a+b
+    def subtract(self, a,b):
+        return a-b
+    def multiply(self, a,b):
+        return a*b
+    def divide(self, a,b):
+        if b==0:
+            return "Error: Division by zero"
+        elif type(a)==int and type(b)==int and a%b==0:
+            return a//b
+        else:
+            return a/b
+            
+calc = Calculator()
+print(calc.add(5, 3))    
+print(calc.divide(10, 0))
