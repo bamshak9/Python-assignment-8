29d18429b6d3301ca33edb34f688c978ca37586e
bamshak9 <b.fwangyil@alustudent.com>
2025-09-06 13:54:49 +0100

    added functionality to AttendanceRegister class in attendance file

--- attendance.py
+++ attendance.py
@@ -14,3 +14,27 @@ Example Usage:
     print(register.get_status("John"))   # "Present"
     print(register.show_register())      # {"John": "Present", "Mary": "Absent"}
 """
+class AttendanceRegister:
+    def __init__(self):
+        self.attendance={}
+    def mark_absent(self,name):
+        self.attendance[name]="Absent"
+        print("Student and Status successfully added")
+    def mark_present(self,name):
+        self.attendance[name]="Present"
+        print("Student and Status successfully added")
+    def get_status(self,name):
+        if name in self.attendance.keys():
+            return self.attendance[name]
+        else:
+            print("Student is not in register")
+    def display_register(self):
+        return self.attendance
+
+
+register = AttendanceRegister()
+print(register.display_register())
+register.mark_present("John")
+register.mark_absent("Mary")
+print(register.get_status("John"))
+print(register.display_register())
